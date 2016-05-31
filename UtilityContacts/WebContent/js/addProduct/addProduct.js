4eb6ae5b77e8c32b8a2eccb6392ad8ad8e35d6c0
Charlie <Charlie@Charlie-PC>
2016-06-01 01:41:56 +0530

    AddProduct Dynamic Dropdown Working

--- UtilityContacts/WebContent/js/addProduct/addProduct.js
+++ UtilityContacts/WebContent/js/addProduct/addProduct.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 	var dropdown = document.getElementById("productType");
 	dropdown.onchange = function(event){
-		alert("Your message"+  $('#productType').val());
         $('#subProductTypePlaceholder').show();
         $("#subProductTypePlaceholder").children().hide(); 
         $('#subProductTypeLabel').show();
