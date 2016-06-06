0ee3c1242f739f97cf33d3e08700e05822ad8815
Nabarun Dey <nabarundeysit@gmail.com>
2016-06-06 13:11:44 +0530

    ViewProduct Spring & ImagesExtract.

--- UtilityContacts/src/viewProduct/projector/ViewProductProjector.java
+++ UtilityContacts/src/viewProduct/projector/ViewProductProjector.java
@@ -1,7 +1,12 @@
 package viewProduct.projector;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import viewProduct.appService.inputBeans.ViewProductAppServiceIB;
 import viewProduct.projector.outputBeans.ViewProductProjectorOB;
+
+import com.databaseBeans.ImagesDBBean;
 
 
 public class ViewProductProjector {
@@ -11,6 +16,12 @@ public class ViewProductProjector {
 		ViewProductProjectorOB viewProductProjectorOB = new ViewProductProjectorOB();
 		viewProductProjectorOB.setProductsDBBean(viewProductAppServiceIB.getProductsDBBean());
 		viewProductProjectorOB.setRentOffersDBBeans(viewProductAppServiceIB.getRentOffersDBBeans());
+		List<String> imagesPathList =new ArrayList<String>();
+		for(ImagesDBBean imagesDBBean : viewProductAppServiceIB.getImagesDBBeans())
+		{
+			imagesPathList.add(imagesDBBean.getImagepath());
+		}
+		viewProductProjectorOB.setProductImagesList(imagesPathList);
 		return viewProductProjectorOB;
 
 	}
