da8b1ac98b6a445c2d739716251ad0fca6a6c87c
Nabarun Dey <nabarundeysit@gmail.com>
2016-06-30 17:51:09 +0530

    PaymentUntested

--- UtilityContacts/src/payment/PaymentAppContext.java
+++ UtilityContacts/src/payment/PaymentAppContext.java
@@ -1,9 +1,22 @@
 package payment;
 
+import payment.projector.outputBeans.PaymentProjectorOB;
+
 
 
 
 
 public class PaymentAppContext {
+	
+	private PaymentProjectorOB paymentProjectorOB;
+
+	public PaymentProjectorOB getPaymentProjectorOB() {
+		return paymentProjectorOB;
+	}
+
+	public void setPaymentProjectorOB(PaymentProjectorOB paymentProjectorOB) {
+		this.paymentProjectorOB = paymentProjectorOB;
+	}
+	
 	
 }
--- UtilityContacts/src/payment/action/PaymentAction.java
+++ UtilityContacts/src/payment/action/PaymentAction.java
@@ -1,8 +1,38 @@
 package payment.action;
 
+import payment.PaymentAppContext;
+import payment.appService.PaymentAppService;
+import payment.appService.inputBeans.PaymentAppServiceIB;
+import payment.projector.outputBeans.PaymentProjectorOB;
+
 
 
 
 public class PaymentAction   {
+	
+	PaymentAppService paymentAppService;
+	PaymentAppContext context;
+	
+	String orderid;
+	String fromusername;
+	String tousername;
+	String amount;
+	
+	public String addPayment()
+	{
+		PaymentAppServiceIB paymentAppServiceIB = new PaymentAppServiceIB();
+		paymentAppServiceIB.setAmount(amount);
+		paymentAppServiceIB.setFromusername(fromusername);
+		paymentAppServiceIB.setOrderid(Integer.parseInt(orderid));
+		paymentAppServiceIB.setTousername(tousername);
+		PaymentProjectorOB paymentProjectorOB = paymentAppService.addPayment(paymentAppServiceIB);
+		context.setPaymentProjectorOB(paymentProjectorOB);
+		return "success";
+	}
+	
+	public String getPaymentsForUser()
+	{
+		return "success";
+	}
 	
 }
