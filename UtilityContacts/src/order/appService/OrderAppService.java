5fab3c0ffc81588769eea51a7e4d45098d00c52a
Charlie <Charlie@Charlie-PC>
2016-09-25 17:51:45 +0530

    Send Mail to Vendor

--- UtilityContacts/src/order/appService/OrderAppService.java
+++ UtilityContacts/src/order/appService/OrderAppService.java
@@ -109,6 +109,9 @@ public class OrderAppService {
 			paymentAppServiceIB.setRentamount(cartItem.getRentAmount());
 			paymentAppServiceIB.setSecuritymoney(cartItem.getSecurityMoney());
 			ProductsDBBean productsDBBean =productsDao.getProductDetails(ordersDBBean.getProductid());
+			MailHandler.orderConfirmationMailVendor(productsDBBean,ordersDBBean);
+			MailHandler.orderConfirmationMailCustomer(productsDBBean,ordersDBBean,userProfile);
+
 			paymentAppServiceIB.setTousername(productsDBBean.getUsername());
 			paymentAppServiceIBs.add(paymentAppServiceIB);
 		}
@@ -116,7 +119,6 @@ public class OrderAppService {
 		cartAppService.emptyCart();
 		OrderProjectorOB orderProjectorOB = orderProjector
 				.confirmOrder(ordersDBBeans);
-		MailHandler.orderConfirmationMail(userProfile.getUserName(),ordersDBBeans,userProfile.getUserName());
 		return orderProjectorOB;
 	}
 
