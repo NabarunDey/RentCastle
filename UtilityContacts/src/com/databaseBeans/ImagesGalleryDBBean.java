3d760819b752a0d1cf8434428c5422d94f6f38ad
Nabarun Dey <nabarundeysit@gmail.com>
2017-02-09 14:23:20 +0530

    Gallery db bean

---
+++ UtilityContacts/src/com/databaseBeans/ImagesGalleryDBBean.java
@@ -0,0 +1,23 @@
+package com.databaseBeans;
+
+public class ImagesGalleryDBBean {
+	
+	private String imagegalleryid;
+	private String imageids;
+	public String getImagegalleryid() {
+		return imagegalleryid;
+	}
+	public void setImagegalleryid(String imagegalleryid) {
+		this.imagegalleryid = imagegalleryid;
+	}
+	public String getImageids() {
+		return imageids;
+	}
+	public void setImageids(String imageids) {
+		this.imageids = imageids;
+	}
+	
+	
+	
+
+}
